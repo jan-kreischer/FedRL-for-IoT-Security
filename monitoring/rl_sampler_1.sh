f45df3adfbd971cf8d4bffe3ec90d5e604edf654
Leitou <timo.schenk@uzh.ch>
2022-06-19 18:14:30 +0200

    add data monitoring prototype 1

--- monitoring/rl_sampler_1.sh
+++ monitoring/rl_sampler_1.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -u
 
-# test
 
 ##############################################################
 #############		SCRIPT CONFIGURATION		##############
@@ -10,10 +9,10 @@ export LC_ALL=C.UTF-8
 #	Events to monitor using perf
 targetEvents="alarmtimer:alarmtimer_fired,alarmtimer:alarmtimer_start,block:block_bio_backmerge,block:block_bio_remap,block:block_dirty_buffer,block:block_getrq,block:block_touch_buffer,block:block_unplug,cachefiles:cachefiles_create,cachefiles:cachefiles_lookup,cachefiles:cachefiles_mark_active,clk:clk_set_rate,cpu-migrations,cs,dma_fence:dma_fence_init,fib:fib_table_lookup,filemap:mm_filemap_add_to_page_cache,gpio:gpio_value,ipi:ipi_raise,irq:irq_handler_entry,irq:softirq_entry,jbd2:jbd2_handle_start,jbd2:jbd2_start_commit,kmem:kfree,kmem:kmalloc,kmem:kmem_cache_alloc,kmem:kmem_cache_free,kmem:mm_page_alloc,kmem:mm_page_alloc_zone_locked,kmem:mm_page_free,kmem:mm_page_pcpu_drain,mmc:mmc_request_start,net:net_dev_queue,net:net_dev_xmit,net:netif_rx,page-faults,pagemap:mm_lru_insertion,preemptirq:irq_enable,qdisc:qdisc_dequeue,random:get_random_bytes,random:mix_pool_bytes_nolock,random:urandom_read,raw_syscalls:sys_enter,raw_syscalls:sys_exit,rpm:rpm_resume,rpm:rpm_suspend,sched:sched_process_exec,sched:sched_process_free,sched:sched_process_wait,sched:sched_switch,sched:sched_wakeup,signal:signal_deliver,signal:signal_generate,skb:consume_skb,skb:kfree_skb,skb:skb_copy_datagram_iovec,sock:inet_sock_set_state,task:task_newtask,tcp:tcp_destroy_sock,tcp:tcp_probe,timer:hrtimer_start,timer:timer_start,udp:udp_fail_queue_rcv_skb,workqueue:workqueue_activate_work,writeback:global_dirty_state,writeback:sb_clear_inode_writeback,writeback:wbc_writepage,writeback:writeback_dirty_inode,writeback:writeback_dirty_inode_enqueue,writeback:writeback_dirty_page,writeback:writeback_mark_inode_dirty,writeback:writeback_pages_written,writeback:writeback_single_inode,writeback:writeback_write_inode,writeback:writeback_written"
 #	Resource monitoring
-resourceMonitor=false
+resourceMonitor=true
 
 #	ransomware
-ransomwareMonitor=false
+ransomwareMonitor=true
 
 #	Time window per sample
 timeWindowSeconds=50
@@ -23,7 +22,8 @@ desiredSamples=99999
 timeAcumulative=0
 
 #	Final and temporal output files
-finalOutput="/tmp/samples_$1_$(date +'%Y-%m-%d-%H-%M')_${timeWindowSeconds}s"
+#finalOutput="/tmp/samples_$1_$(date +'%Y-%m-%d-%H-%M')_${timeWindowSeconds}s" # in case of calling with a parameter
+finalOutput="samples_$(date +'%Y-%m-%d-%H-%M')_${timeWindowSeconds}s"
 tempOutput=temp
 
 ##############################################################
