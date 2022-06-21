03231e9c66bb99ae9e527ed24caa2b219441281a
Leitou <timo.schenk@uzh.ch>
2022-06-21 10:05:07 +0200

    add ransom data; add ae; add supervisor map

---
+++ 1-prototype/episode_generator.py
@@ -0,0 +1,4 @@
+
+# TODO:
+#  read in data
+#  preprocess
---
+++ custom_types.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+
+class Behavior(Enum):
+    NORMAL = "normal"
+    ROOTKIT_BDVL = "bdvl"
+    ROOTKIT_BEURK = "beurk"
+    CNC_BACKDOOR_JAKORITAR = "backdoor_jakoritar"
+    CNC_THETICK = "the_tick"
+    RANSOMWARE_POC = "ransomware_poc"
+
+
+class MTDTechnique(Enum):
+    ROOTKIT_SANITIZER: "rootkit_sanitizer"
+    RANSOMWARE_DIRTRAP: "ransomware_directory_trap"
+    RANSOMWARE_FILE_EXT_HIDE: "ransomware_file_extension_hide"
+    CNC_IP_SHUFFLE: "cnc_ip_shuffle"
+    NO_MTD: "no_mtd"
