611ac1cc7be3be884089bdaf470b6ecfdfc8f65e
Leitou <timo.schenk@uzh.ch>
2022-07-18 16:10:52 +0200

    add loading of pretrained ae

--- autoencoder.py
+++ autoencoder.py
@@ -1,13 +1,24 @@
 from torch import nn
 import torch
 import numpy as np
+from tqdm import tqdm
 
 
-class AutoEncoderInterpreter():
+def auto_encoder_model(in_features: int, hidden_size: int = 6):
+    return nn.Sequential(
+        nn.Linear(in_features, hidden_size),
+        nn.BatchNorm1d(hidden_size),
+        nn.GELU(),
+        nn.Linear(hidden_size, in_features),
+        nn.GELU()
+    )
+
+class AutoEncoder():
 
     def __init__(self, train_x: np.ndarray, train_y: np.ndarray,
                  valid_x: np.ndarray, valid_y: np.ndarray,
                  batch_size: int = 64, batch_size_valid=64):
+
         data_train = torch.utils.data.TensorDataset(
             torch.from_numpy(train_x).type(torch.float),
             torch.from_numpy(train_y).type(torch.float)
@@ -21,37 +32,31 @@ class AutoEncoderInterpreter():
         self.valid_loader = torch.utils.data.DataLoader(data_valid, batch_size=batch_size_valid, shuffle=True)
         self.validation_losses = []
 
-        self.model = self.auto_encoder_model(in_features=train_x.shape[1])
+        self.model = auto_encoder_model(in_features=train_x.shape[1])
         self.threshold = np.nan
 
+    def get_model(self):
+        return self.model
 
-    def auto_encoder_model(self, in_features: int, hidden_size: int = 32):
-        return nn.Sequential(
-            nn.Linear(in_features, hidden_size),
-            nn.BatchNorm1d(hidden_size),
-            nn.GELU(),
-            nn.Linear(hidden_size, in_features),
-            nn.GELU()
-        )
-
-
-    def train(self, optimizer, loss_function, num_epochs: int = 15):
+    def train(self, optimizer, loss_function=torch.nn.MSELoss(reduction='sum'), num_epochs: int = 15):
         if self.model is None:
             raise ValueError("No model set!")
+
         epoch_losses = []
-        for le in range(num_epochs):
-            self.model.train()
-            current_losses = []
-            for batch_idx, (x, _) in enumerate(self.data_loader):
-                x = x  # x.cuda()
-                optimizer.zero_grad()
-                model_out = self.model(x)
-                loss = loss_function(model_out, x)
-                loss.backward()
-                optimizer.step()
-                current_losses.append(loss.item())
-            epoch_losses.append(sum(current_losses) / len(current_losses))
-            # print(f'Training Loss in epoch {le + 1}: {epoch_losses[le]}')
+        for _ in tqdm(range(num_epochs), unit="epoch", leave=False):
+            for le in range(num_epochs):
+                self.model.train()
+                current_losses = []
+                for batch_idx, (x, _) in enumerate(self.data_loader):
+                    x = x  # x.cuda()
+                    optimizer.zero_grad()
+                    model_out = self.model(x)
+                    loss = loss_function(model_out, x)
+                    loss.backward()
+                    optimizer.step()
+                    current_losses.append(loss.item())
+                epoch_losses.append(sum(current_losses) / len(current_losses))
+                # print(f'Training Loss in epoch {le + 1}: {epoch_losses[le]}')
 
     def determine_threshold(self) -> float:
         mses = []
@@ -64,4 +69,21 @@ class AutoEncoderInterpreter():
                 loss = loss_function(model_out, x)
                 mses.append(loss.item())
         mses = np.array(mses)
-        return mses.mean() + 3 * mses.std()
+        self.threshold = mses.mean() + 3 * mses.std()
+        return self.threshold
+
+    def save_model(self):
+        torch.save({
+            'model_state_dict': self.model.state_dict(),
+            'threshold': self.threshold
+        }, "trained_models/autoencoder_model.pth")
+
+
+class AutoEncoderInterpreter():
+    def __init__(self, state_dict, threshold, in_features=15, hidden_size=6):
+        self.model = auto_encoder_model(in_features=in_features, hidden_size=hidden_size)
+        self.model.load_state_dict(state_dict)
+        self.threshold = threshold
+
+    def predict(self, x):
+        pass
