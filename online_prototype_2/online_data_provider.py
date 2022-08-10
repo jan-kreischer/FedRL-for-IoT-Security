7000a17825c27c02c4343a61dedbb2ed0438823f
Leitou <timo.schenk@uzh.ch>
2022-08-10 19:00:38 +0200

    quickfix remove nan-columns from online data

--- online_prototype_2/online_data_provider.py
+++ online_prototype_2/online_data_provider.py
@@ -8,29 +8,33 @@ import pandas as pd
 import os
 import joblib
 
-
 time_status_columns = ["time", "timestamp", "seconds", "connectivity"]
 all_zero_columns = ['cpuNice', 'cpuHardIrq', 'alarmtimer:alarmtimer_fired',
-       'alarmtimer:alarmtimer_start', 'cachefiles:cachefiles_create',
-       'cachefiles:cachefiles_lookup', 'cachefiles:cachefiles_mark_active',
-       'dma_fence:dma_fence_init', 'udp:udp_fail_queue_rcv_skb']
-    #
-    #
-    # ["alarmtimer:alarmtimer_fired", "alarmtimer:alarmtimer_start", "cachefiles:cachefiles_create",
-    #                 "cachefiles:cachefiles_lookup", "cachefiles:cachefiles_mark_active", "dma_fence:dma_fence_init",
-    #                 "udp:udp_fail_queue_rcv_skb"]
+                    'alarmtimer:alarmtimer_start', 'cachefiles:cachefiles_create',
+                    'cachefiles:cachefiles_lookup', 'cachefiles:cachefiles_mark_active',
+                    'dma_fence:dma_fence_init', 'udp:udp_fail_queue_rcv_skb']
+#
+#
+# ["alarmtimer:alarmtimer_fired", "alarmtimer:alarmtimer_start", "cachefiles:cachefiles_create",
+#                 "cachefiles:cachefiles_lookup", "cachefiles:cachefiles_mark_active", "dma_fence:dma_fence_init",
+#                 "udp:udp_fail_queue_rcv_skb"]
+
+perf_5_10_nan_columns = ['filemap:mm_filemap_add_to_page_cache', 'kmem:mm_page_alloc',
+                         'kmem:mm_page_alloc_zone_locked', 'kmem:mm_page_free', 'kmem:mm_page_pcpu_drain']
+perf_5_10_nan_columns += ['ramFree', 'ramUsed', 'writeback:writeback_write_inode', 'writeback:writeback_written']
 
 # choose which data to use for the scaler/pca
 all_file = "all_data_filtered_external_True_constant_True_outliers_True.csv"
 pca_file = "data_transforms/pcafit.gz"
 scaler_file = "data_transforms/scaler.gz"
 
+
 class OnlineDataProvider:
     @staticmethod
     def __parse_file_to_df(file_name, filter_suspected_external_events=True,
-                              filter_constant_columns=True,
-                              filter_outliers=True,
-                              keep_status_columns=False) -> pd.DataFrame:
+                           filter_constant_columns=True,
+                           filter_outliers=True,
+                           keep_status_columns=False) -> pd.DataFrame:
 
         df = pd.read_csv(file_name)
 
@@ -43,6 +47,9 @@ class OnlineDataProvider:
         # remove model-irrelevant columns
         if not keep_status_columns:
             df = df.drop(time_status_columns, axis=1)
+        # only in online setting nan columns that are not available as events
+        df = df.drop(perf_5_10_nan_columns, axis=1)
+
         if filter_outliers:
             # drop outliers per measurement, indicated by (absolute z score) > 3
             df = df[(np.nan_to_num(np.abs(stats.zscore(df))) < 3).all(axis=1)]
@@ -50,16 +57,17 @@ class OnlineDataProvider:
         if filter_constant_columns:
             df = df.drop(all_zero_columns, axis=1)
 
-        #df.to_csv(file_name + "preprocessed", index_label=False)
+        # df.to_csv(file_name + "preprocessed", index_label=False)
         return df
 
     @staticmethod
     def __get_scaler_and_pca(scaling_minmax=True, dim=15):
-        all_filtered_data = pd.read_csv(all_file).to_numpy()[:, :-1]
+        all_filtered_data = pd.read_csv(all_file).drop(perf_5_10_nan_columns, axis=1).to_numpy()[:, :-1].astype(np.float64)
         scaler = StandardScaler() if not scaling_minmax else MinMaxScaler()
         scaler.fit(all_filtered_data)
+        all_scaled = scaler.transform(all_filtered_data)
         pca = PCA(n_components=dim)
-        pca.fit(all_filtered_data)
+        pca.fit(all_scaled)
 
         if not os.path.isfile(scaler_file) or not os.path.isfile(pca_file):
             joblib.dump(scaler, scaler_file)
@@ -84,6 +92,3 @@ class OnlineDataProvider:
             #     pca = pickle.load(pf)
 
         return pca.transform(scaler.transform(data))
-
-
-
