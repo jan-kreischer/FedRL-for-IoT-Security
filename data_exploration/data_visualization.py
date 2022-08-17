2c21e4ada67ddc9282b6cfb6461049cc9b260267
Leitou <timo.schenk@uzh.ch>
2022-08-17 15:43:40 +0200

    add new decision/afterstate comp plots

--- data_exploration/data_visualization.py
+++ data_exploration/data_visualization.py
@@ -21,27 +21,27 @@ if __name__ == "__main__":
              (Behavior.RANSOMWARE_POC, MTDTechnique.RANSOMWARE_FILE_EXT_HIDE, "lightblue"),
              (Behavior.CNC_BACKDOOR_JAKORITAR, MTDTechnique.CNC_IP_SHUFFLE, "orange"),
              (Behavior.ROOTKIT_BDVL, MTDTechnique.ROOTKIT_SANITIZER, "red")],
-            plot_name="dac_normal_behavior_to_correct_MTDs_afterstate_timeline_comparison")
+            plot_name="dac_normal_behavior_to_correct_MTDs_afterstate_timeline")
 
         DataPlotter.plot_decision_afterstate_comparison(
             [(Behavior.RANSOMWARE_POC, "green")],
             [(Behavior.RANSOMWARE_POC, MTDTechnique.CNC_IP_SHUFFLE, "blue"),
              (Behavior.RANSOMWARE_POC, MTDTechnique.ROOTKIT_SANITIZER, "lightblue")],
-            plot_name="dac_ransom_behavior_to_incorrect_MTDs_afterstate_timeline_comparison")
+            plot_name="dac_ransom_behavior_to_incorrect_MTDs_afterstate_timeline")
 
         DataPlotter.plot_decision_afterstate_comparison(
             [(Behavior.CNC_BACKDOOR_JAKORITAR, "green")],
             [(Behavior.CNC_BACKDOOR_JAKORITAR, MTDTechnique.RANSOMWARE_DIRTRAP, "blue"),
              (Behavior.CNC_BACKDOOR_JAKORITAR, MTDTechnique.RANSOMWARE_FILE_EXT_HIDE, "lightblue"),
              (Behavior.CNC_BACKDOOR_JAKORITAR, MTDTechnique.ROOTKIT_SANITIZER, "orange")],
-            plot_name="dac_cnc_behavior_to_incorrect_MTDs_afterstate_timeline_comparison")
+            plot_name="dac_cnc_behavior_to_incorrect_MTDs_afterstate_timeline")
 
         DataPlotter.plot_decision_afterstate_comparison(
             [(Behavior.ROOTKIT_BDVL, "green")],
             [(Behavior.ROOTKIT_BDVL, MTDTechnique.RANSOMWARE_DIRTRAP, "blue"),
              (Behavior.ROOTKIT_BDVL, MTDTechnique.RANSOMWARE_FILE_EXT_HIDE, "lightblue"),
              (Behavior.ROOTKIT_BDVL, MTDTechnique.CNC_IP_SHUFFLE, "orange")],
-            plot_name="dac_rootkit_behavior_to_incorrect_MTDs_afterstate_timeline_comparison")
+            plot_name="dac_rootkit_behavior_to_incorrect_MTDs_afterstate_timeline")
 
         # DataPlotter.plot_behaviors(
         #     [(RaspberryPi.PI4_2GB_WC, Behavior.NORMAL, "blue"),
