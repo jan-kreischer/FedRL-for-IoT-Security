5f07d50cd24d56bafb6bd1e7c793e1d1c6aa8019
Jan Kreischer <jan.kr.bauer@gmail.com>
2023-04-02 16:54:51 +0200

    Implemented python multiprocessing (pool)

---
+++ prototypes/prototype_01/client.py
@@ -0,0 +1,175 @@
+import torch
+from torch import nn, optim
+import copy
+import numpy as np
+import matplotlib.pyplot as plt
+from tabulate import tabulate
+import matplotlib.pyplot as plt
+from time import time, time_ns
+from agent import Agent
+from sensor_environment import SensorEnvironment
+from src.custom_types import MTDTechnique, Behavior
+
+#%matplotlib inline
+
+class Client:
+        # memory buffer is influenced by env.step -> resetting to previous action, which may result in unbalanced training
+    def __init__(self, client_id: int, agent: Agent, environment: SensorEnvironment, save_path="",  min_replay_size=100):
+        self.client_id = client_id
+        self.agent = agent
+        self.environment = environment
+        self.episode_returns = [] 
+        self.eps_history = []
+        self.save_path = save_path
+        self.start_time = 0
+        self.end_time = 0
+        
+        # initialize replay memory
+        self.init_replay_memory(min_replay_size)
+    
+    def init_replay_memory(self, min_size):
+        obs = self.environment.reset()
+        episode_action_memory = []
+        i = 0
+        while i < min_size:
+            try:
+                action = np.random.choice(list({0,1,2,3}.difference(episode_action_memory)))
+                episode_action_memory.append(action)
+            except ValueError:
+                obs = self.environment.reset()
+                episode_action_memory = []
+                # results in slightly less entries than min_size
+                print("exhausted all mtd techniques")
+                continue
+            i += 1
+
+            new_obs, reward, done = self.environment.step(action)
+            idx1 = -1 if obs[0, -1] in Behavior else -2
+            idx2 = -1 if new_obs[0, -1] in Behavior else -2
+            transition = (obs[:, :idx1], action, reward, new_obs[:, :idx2], done)
+            self.agent.replay_buffer.append(transition)
+
+            obs = new_obs
+            if done:
+                obs = self.environment.reset()
+                episode_action_memory = []
+
+                
+    def train_agent(self, num_episodes, t_update_freq=100, verbose=False):
+        self.start_time = time()
+        step = 0
+        for num_episode in range(num_episodes):
+            episode_return = 0
+            episode_steps = 0
+            done = False
+            obs = self.environment.reset()
+            while not done:
+                idx1 = -1 if obs[0, -1] in Behavior else -2
+                action = self.agent.choose_action(obs[:, :idx1])
+                if action == -1:
+                    print("Agent exhausted all MTD techniques upon behavior: ", obs[0, -1])
+                    self.agent.episode_action_memory = set()
+                    done = True
+                    continue
+
+                new_obs, reward, done = self.environment.step(action)
+                idx2 = -1 if new_obs[0, -1] in Behavior else -2
+                episode_return += reward
+                self.agent.replay_buffer.append((obs[:, :idx1], action, reward,
+                                            new_obs[:, :idx2], done))
+                self.agent.reward_buffer.append(reward)
+                if done:
+                    self.agent.episode_action_memory = set()
+
+                self.agent.learn()
+                obs = new_obs
+
+                episode_steps += 1
+                # update target network
+                step += 1
+                if step % t_update_freq == 0:
+                    self.agent.update_target_network()
+
+            self.episode_returns.append(episode_return / episode_steps)
+            self.eps_history.append(self.agent.epsilon)
+         
+        self.end_time = time() 
+        #print(f"The total training time on {self.agent.get_name()} ({num_episodes}) was {self.get_training_time()}s")
+        
+    
+    def receive_weights(self, model_params):
+        self.agent.update_weights(model_params)
+        
+    def get_weights(self):
+        return self.agent.get_weights()
+    
+    def get_training_summary(self):
+        return self.episode_returns, self.eps_history
+    
+
+    def plot_learning_curve(self, filename, nr_round: int):
+        title = f"Learning Curve until Round {nr_round} on Client {self.client_id}"
+        returns =  []
+        epsilons = []
+        returns, epsilons = self.get_training_summary()
+
+        assert len(returns) == len(epsilons)
+        x = [i + 1 for i in range(len(returns))]
+        fig = plt.figure()
+
+        ax = fig.add_subplot(111, label="1")
+        ax2 = fig.add_subplot(111, label="2", frame_on=False)
+        ax.set_title(title)
+
+        color_1 = "red"
+        ax.plot(x, epsilons, color=color_1)
+        ax.set_xlabel("Episode", color='black')
+        ax.set_ylabel("Epsilon", color=color_1)
+        ax.set_ylim([0, 1])
+        ax.tick_params(axis='x', colors='black')
+        ax.tick_params(axis='y', colors=color_1)
+        
+        N = len(returns)
+        running_avg = np.empty(N)
+        for t in range(N):
+            running_avg[t] = np.mean(returns[max(0, t - 20):(t + 1)])
+        
+        color_2 = "blue"
+        ax2.scatter(x, running_avg, color=color_2, s=2 ** 2)
+        ax2.axes.get_xaxis().set_visible(False)
+        ax2.yaxis.tick_right()
+        ax2.set_ylabel('Average Return', color=color_2)
+        ax2.set_ylim([0, 1])
+        ax2.yaxis.set_label_position('right')
+        ax2.tick_params(axis='y', colors=color_2)
+
+        if self.save_path:
+            if not os.path.isdir(self.save_path):
+                os.makedirs(self.save_path)
+            filepath = os.path.join(self.save_path, filename)
+            plt.savefig(filepath)
+            
+        plt.show()
+        
+    def get_training_time(self):
+        return self.end_time - self.start_time
+    
+    def plot_training_data_split(self):
+        D = {}
+        for key, value in self.environment.train_data.items():
+            name = str(key).split('.')[1]
+            D[name] = len(value)
+        
+        plt.bar(range(len(D)), list(D.values()), align='center', color='blue')
+        plt.xticks(range(len(D)), list(D.keys()))
+        plt.xticks(rotation=45, ha='right')
+        plt.title(f"Behavior Distribution on Client {self.client_id}")
+        plt.ylabel('n_samples')
+        
+        filename = f"behavior_sample_distribution_on_client-{self.client_id:02d}.png"
+        filepath = os.path.join(self.save_path, filename)
+        plt.savefig(filepath, bbox_inches="tight")
+        plt.show()
+        
+    #def plot_performance_curve():
+        
