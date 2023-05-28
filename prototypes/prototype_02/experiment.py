4a997da15fcc1f0eede0bdb81cbc7a45a719880a
Jan Kreischer <jan.kr.bauer@gmail.com>
2023-05-28 11:09:33 +0200

    Safety commit before re-running prototype-02

---
+++ prototypes/prototype_02/experiment.py
@@ -0,0 +1,202 @@
+import numpy as np
+import os
+import shutil
+import matplotlib.pyplot as plt
+from src.custom_types import Behavior
+
+class Experiment:
+    def __init__(self, base_path, experiment_id, experiment_version):
+        self.base_path = base_path
+        self.experiment_id = experiment_id
+        self.experiment_version = experiment_version
+        
+    def get_experiment_path(self, overwrite=False):
+        path = os.path.join(self.base_path, f"experiments/experiment_{self.experiment_id:02d}")
+        if self.experiment_version != 0:
+            path = os.path.join(path, f"version_{self.experiment_version:02d}")
+         
+        if overwrite & os.path.exists(path):
+            shutil.rmtree(path)          
+            os.makedirs(path)
+            
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        return path
+    
+    def add_server(self, server):
+        self.server = server
+        
+    def execute(self, 
+                server,
+                nr_rounds: int,
+                nr_episodes_per_round: int,
+                evaluations,
+                evaluation_frequency=1):
+        print(f"=== STARTING EXPERIMENT {self.experiment_id}.{self.experiment_version} ===\n")
+        self.server = server
+        self.nr_rounds = nr_rounds
+        self.nr_episodes_per_round = nr_episodes_per_round
+        self.server.run_federation(nr_rounds=nr_rounds, nr_episodes_per_round=nr_episodes_per_round, evaluations=evaluations, evaluation_frequency=evaluation_frequency)
+        
+    def plot_test_performances(self, show_episodes = True, y_threshold=99):
+        agents = list(map(lambda client: client.agent, self.server.clients))
+        agents.append(self.server.global_agent)
+
+        plt.figure(figsize=(9,6))
+        for agent in agents:
+            #print(agent.get_name())
+            #print(agent.total_accuracies)
+            #print(agent.mean_class_accuracies)
+            #print("---------")
+            #plt.ylim(0, 0.9)
+            agent_id = agent.agent_id
+
+            label = agent.get_name()
+            x = list(agent.total_accuracies.keys())
+            if len(x) == 0:
+                continue
+            if show_episodes:
+                x = np.array(x)*self.nr_episodes_per_round
+                x_label = "Number of Episodes trained per Client"
+            else:
+                x_label = "Number of Rounds"
+                
+            y = np.array(list(agent.total_accuracies.values()))*100
+            if agent.agent_id == 0:  
+                y_index = np.argmax(y>y_threshold)
+            
+            if agent_id == 0:
+                color='blue'
+                linestyle='solid'
+                linewidth=2
+            else:
+                color='#666666'
+                linestyle='dashdot'
+                linewidth=1
+                #plt.plot(x, y, label=label, color=color, linestyle=linestyle, linewidth=linewidth)
+            plt.plot(x, y, label=label, color=color, linestyle=linestyle, linewidth=linewidth)
+
+            #plt.plot(x, y, label=label, color=color, linestyle=linestyle, linewidth=linewidth)
+        plt.xticks(x)
+        plt.xticks(rotation=45)
+        #ax = plt.axes()        
+        #ax.yaxis.grid()
+        #plt.grid()
+        plt.xlabel(x_label)
+        plt.ylabel('Test Accuracy [%]')
+        plt.ylim(0, 105)
+        plt.title("Micro Test Accuracy over multiple Rounds")
+        if y_index != 0 or y[0] > y_threshold:
+            plt.axvline(x=(y_index+1)*self.nr_episodes_per_round, color='black', linestyle='dotted')
+        plt.axhline(y=y_threshold, color='black', linestyle='dashed', label=f"{y_threshold}% Accuracy Threshold")
+        plt.legend(loc="lower right")
+        plt.show()
+        
+    def plot_class_weighted_test_accuracy(self, show_episodes = True, y_threshold=99):
+        agents = list(map(lambda client: client.agent, self.server.clients))
+        agents.append(self.server.global_agent)
+
+        plt.figure(figsize=(9,6))
+        for agent in agents:
+            #print(agent.get_name())
+            #print(agent.total_accuracies)
+            #print(agent.mean_class_accuracies)
+            #print("---------")
+            #plt.ylim(0, 0.9)
+            agent_id = agent.agent_id
+
+            label = agent.get_name()
+            x = list(agent.mean_class_accuracies.keys())
+            if len(x) == 0:
+                continue
+            if show_episodes:
+                x = np.array(x)*self.nr_episodes_per_round
+                x_label = "Number of Episodes trained per Client"
+            else:
+                x_label = "Number of Rounds" 
+            y = np.array(list(agent.mean_class_accuracies.values()))*100
+            if agent.agent_id == 0:  
+                y_index = np.argmax(y>y_threshold)
+            
+            if agent_id == 0:
+                color='blue'
+                linestyle='solid'
+                linewidth=2
+            else:
+                color='#666666'
+                linestyle='dashdot'
+                linewidth=1
+                #plt.plot(x, y, label=label, color=color, linestyle=linestyle, linewidth=linewidth)
+            plt.plot(x, y, label=label, color=color, linestyle=linestyle, linewidth=linewidth)
+
+            #plt.plot(x, y, label=label, color=color, linestyle=linestyle, linewidth=linewidth)
+        plt.xticks(x)
+        plt.xticks(rotation=45)
+        #ax = plt.axes()        
+        #ax.yaxis.grid()
+        #plt.grid()
+        plt.xlabel(x_label)
+        plt.ylabel('Test Accuracy [%]')
+        plt.ylim(0, 105)
+        plt.title("Macro Test Accuracy over multiple Rounds")
+        if y_index != 0 or y[0] > y_threshold:
+            plt.axvline(x=(y_index+1)*self.nr_episodes_per_round, color='black', linestyle='dotted')
+        plt.axhline(y=y_threshold, color='black', linestyle='dashed', label=f"{y_threshold}% Accuracy Threshold")
+        plt.legend(loc="lower right")
+        plt.show()
+        
+    def plot_behavior_performances(self, show_episodes = True, y_threshold=99):
+        agents = list(map(lambda client: client.agent, self.server.clients))
+        agents.append(self.server.global_agent)
+
+        non_normal_behaviors = [behavior for behavior in Behavior if behavior is not Behavior.NORMAL]
+        for behavior in non_normal_behaviors:
+            plt.figure(figsize=(9,6))
+            for agent in agents:
+                #print(agent.get_name())
+                #print(agent.total_accuracies)
+                #print(agent.mean_class_accuracies)
+                #print("---------")
+                agent_id = agent.agent_id
+
+                label = agent.get_name()
+                x = list(agent.behavior_accuracies[behavior].keys())
+                if len(x) == 0:
+                    continue
+                if show_episodes:
+                    x = np.array(x)*self.nr_episodes_per_round
+                    x_label = "Number of Episodes trained per Client"
+                else:
+                    x_label = "Number of Rounds"
+                y = np.array(list(agent.behavior_accuracies[behavior].values()))*100
+                
+                if agent.agent_id == 0:  
+                    y_index = np.argmax(y>y_threshold)
+
+                if agent_id == 0:
+                    color='blue'
+                    linestyle='solid'
+                    linewidth=2
+                else:
+                    color='#666666'
+                    linestyle='dashdot'
+                    linewidth=1
+                    #plt.plot(x, y, label=label, color=color, linestyle=linestyle, linewidth=linewidth)
+                plt.plot(x, y, label=label, color=color, linestyle=linestyle, linewidth=linewidth)
+
+                #plt.plot(x, y, label=label, color=color, linestyle=linestyle, linewidth=linewidth)
+            plt.xticks(x)
+            plt.xticks(rotation=45)
+            plt.xlabel(x_label)
+            plt.ylabel('Test Accuracy [%]')
+            #plt.grid()
+            #ax = plt.axes()        
+            #ax.yaxis.grid()
+            plt.ylim(0, 105)
+            plt.title(f"Test Accuracy for {behavior.name} over multiple Rounds")
+            if y_index != 0 or y[0] > y_threshold:
+                plt.axvline(x=(y_index+1)*self.nr_episodes_per_round, color='black', linestyle='dotted')
+            plt.axhline(y=y_threshold, color='black', linestyle='dashed', label=f"{y_threshold}% Accuracy Threshold")
+            plt.legend(loc="lower right")
+            plt.show()
--- src/custom_types.py
+++ src/custom_types.py
@@ -1,9 +1,5 @@
 from enum import Enum, auto
 
-class RaspberryPi(Enum):
-    PI4_2GB_WC = "pi_4_2gb"
-    PI3_1GB = "pi_3_1gb"
-
 class Behavior(Enum):
     NORMAL = "normal"
     ROOTKIT_BDVL = "bdvl"
@@ -14,18 +10,6 @@ class Behavior(Enum):
     CNC_OPT2 = "data_leak_2"
     RANSOMWARE_POC = "ransomware_poc"
    
-'''
-class Behavior(Enum):
-    NORMAL = auto()
-    ROOTKIT_BDVL = auto()
-    ROOTKIT_BEURK = auto()
-    CNC_BACKDOOR_JAKORITAR = auto()
-    CNC_THETICK = auto()
-    CNC_OPT1 = auto()
-    CNC_OPT2 = auto()
-    RANSOMWARE_POC = auto()
-'''
-
 
 class MTDTechnique(Enum):
     CNC_IP_SHUFFLE = "cnc_ip_shuffle"
@@ -33,9 +17,6 @@ class MTDTechnique(Enum):
     RANSOMWARE_DIRTRAP = "ransomware_directory_trap"
     RANSOMWARE_FILE_EXT_HIDE = "ransomware_file_extension_hide"
       
-
-# define MTD - (target Attack) Mapping
-# indices of supervisor_map corresponding to sequence in "actions"
 actions = [
     MTDTechnique.CNC_IP_SHUFFLE,
     MTDTechnique.ROOTKIT_SANITIZER,
@@ -43,28 +24,12 @@ actions = [
     MTDTechnique.RANSOMWARE_FILE_EXT_HIDE
 ]
 
-# noinspection PyTypeChecker
-# Mapping between chosen MTD and which attacks can be mitigated by this MTD
 mitigated_by = {
     0: [Behavior.CNC_BACKDOOR_JAKORITAR, Behavior.CNC_THETICK, Behavior.CNC_OPT1, Behavior.CNC_OPT2],
     1: [Behavior.ROOTKIT_BDVL, Behavior.ROOTKIT_BEURK],
     2: [Behavior.RANSOMWARE_POC],
     3: [Behavior.RANSOMWARE_POC],
 }
-
-
-'''
-# noinspection PyTypeChecker
-# Mapping between chosen MTD and which attacks can be mitigated by this MTD
-supervisor_map = {
-    # MTDTechnique.NO_MTD: (Behavior.NORMAL,),
-    0: [Behavior.NORMAL],
-    1: [Behavior.CNC_BACKDOOR_JAKORITAR, Behavior.CNC_THETICK, Behavior.CNC_OPT1, Behavior.CNC_OPT2],
-    2: [Behavior.ROOTKIT_BDVL, Behavior.ROOTKIT_BEURK],
-    3: [Behavior.RANSOMWARE_POC],
-    4: [Behavior.RANSOMWARE_POC],
-}
-'''
 
 normal_afterstates = [
     (Behavior.ROOTKIT_BDVL, MTDTechnique.ROOTKIT_SANITIZER),
@@ -76,56 +41,3 @@ normal_afterstates = [
     (Behavior.CNC_OPT1, MTDTechnique.CNC_IP_SHUFFLE),
     (Behavior.CNC_OPT2, MTDTechnique.CNC_IP_SHUFFLE),
 ]
-
-'''
-from enum import Enum
-
-
-class RaspberryPi(Enum):
-    PI4_2GB_WC = "pi_4_2gb"
-    PI3_1GB = "pi_3_1gb"
-
-class Behavior(Enum):
-    NORMAL = "normal"
-    ROOTKIT_BDVL = "bdvl"
-    ROOTKIT_BEURK = "beurk"
-    CNC_BACKDOOR_JAKORITAR = "backdoor_jakoritar"
-    CNC_THETICK = "the_tick"
-    CNC_OPT1 = "data_leak_1"
-    CNC_OPT2 = "data_leak_2"
-    RANSOMWARE_POC = "ransomware_poc"
-    
-class MTDTechnique(Enum):
-    ROOTKIT_SANITIZER = "rootkit_sanitizer"
-    RANSOMWARE_DIRTRAP = "ransomware_directory_trap"
-    RANSOMWARE_FILE_EXT_HIDE = "ransomware_file_extension_hide"
-    CNC_IP_SHUFFLE = "cnc_ip_shuffle"
-    CONTINUE = "continue"
-    #NO_MTD = "no_mtd"
-
-
-# define MTD - (target Attack) Mapping
-# indices of supervisor_map corresponding to sequence in "actions"
-actions = (MTDTechnique.CNC_IP_SHUFFLE, MTDTechnique.ROOTKIT_SANITIZER,
-           MTDTechnique.RANSOMWARE_DIRTRAP, MTDTechnique.RANSOMWARE_FILE_EXT_HIDE)
-
-# noinspection PyTypeChecker
-supervisor_map = {
-    # MTDTechnique.NO_MTD: (Behavior.NORMAL,),
-    0: (Behavior.CNC_BACKDOOR_JAKORITAR, Behavior.CNC_THETICK, Behavior.CNC_OPT1, Behavior.CNC_OPT2, Behavior.NORMAL),
-    1: (Behavior.ROOTKIT_BDVL, Behavior.ROOTKIT_BEURK, Behavior.NORMAL),
-    2: (Behavior.RANSOMWARE_POC, Behavior.NORMAL),
-    3: (Behavior.RANSOMWARE_POC, Behavior.NORMAL)
-}
-
-normal_afterstates = (
-    (Behavior.ROOTKIT_BDVL, MTDTechnique.ROOTKIT_SANITIZER),
-    (Behavior.ROOTKIT_BEURK, MTDTechnique.ROOTKIT_SANITIZER),
-    (Behavior.RANSOMWARE_POC, MTDTechnique.RANSOMWARE_DIRTRAP),
-    (Behavior.RANSOMWARE_POC, MTDTechnique.RANSOMWARE_FILE_EXT_HIDE),
-    (Behavior.CNC_BACKDOOR_JAKORITAR, MTDTechnique.CNC_IP_SHUFFLE),
-    (Behavior.CNC_THETICK, MTDTechnique.CNC_IP_SHUFFLE),
-    (Behavior.CNC_OPT1, MTDTechnique.CNC_IP_SHUFFLE),
-    (Behavior.CNC_OPT2, MTDTechnique.CNC_IP_SHUFFLE),
-)
-'''
