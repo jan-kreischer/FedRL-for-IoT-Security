37783201fa1619e71ab6306ef471108c3c914fc9
Jan Kreischer <jan.kr.bauer@gmail.com>
2023-06-09 23:53:22 +0200

    test scaling only on normal data for anomaly detection

--- anomaly_detection_tests.py
+++ anomaly_detection_tests.py
@@ -30,14 +30,19 @@ if __name__ == '__main__':
     ae_train_dict, atrain_rl = DataProvider.split_as_data_for_ae_and_rl(atrain)
 
     # read in all data
+    ntrain, test_ddata, test_adata, scaler = DataProvider.get_scaled_train_test_split_anomaly_detection_afterstates()
+
     # create train/test split & scale
     # pretrain
+
+
 
     # fit diverse classifiers and test them
 
     # LocalOutlierFactor
     clf = LocalOutlierFactor(n_neighbors=40, novelty=True, contamination=0.1)
-    clf.fit(ae_ds_train[:, :-1])
+    clf.fit(ntrain[:, :-1])
+    #clf.fit(ae_ds_train[:, :-1])
 
     # IsolationForest
     # clf = IsolationForest(n_estimators=15, random_state=0)
@@ -54,6 +59,8 @@ if __name__ == '__main__':
     # # n_error_train = y_pred_train[y_pred_train == -1].size
 
     print("Evaluate on decision state data")
-    evaluate_anomaly_detector_ds(dtrain_rl, clf)
+    #evaluate_anomaly_detector_ds(dtrain_rl, clf)
+    evaluate_anomaly_detector_ds(test_ddata, clf)
     print("Evaluate on afterstate data")
-    evaluate_anomaly_detector_as(atrain_rl, clf)
+    #evaluate_anomaly_detector_as(atrain_rl, clf)
+    evaluate_anomaly_detector_as(test_adata, clf)
