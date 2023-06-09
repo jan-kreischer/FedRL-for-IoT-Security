4c3d348609fbf502f9d4c6007560f36251bb6bc6
Jan Kreischer <jan.kr.bauer@gmail.com>
2023-06-09 23:52:56 +0200

    add basic env/agent structure

---
+++ prototype_1/environment.py
@@ -0,0 +1,28 @@
+class Environment():
+    def __init__(self):
+        pass
+
+    def step(self):
+        """
+        function needs to return:
+        new_state, reward, isTerminalState
+        """
+
+        pass
+
+    def reset(self):
+        pass
+
+
+    def calculate_reward(self):
+        """
+        if action == supervisor_map[state.behavior]:
+        then return positive
+        else return negative
+
+        test_afterstate_for_normality()
+        if ae.predict() == Normal:
+        then return positive
+        else return negative
+        """
+        return 0
