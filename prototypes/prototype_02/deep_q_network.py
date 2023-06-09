1428edd96d1dbf88fc683bce5c5245aaaab94c8f
Jan Kreischer <jan.kr.bauer@gmail.com>
2023-06-09 23:53:38 +0200

    Commit before removing document functionality

--- prototypes/prototype_02/deep_q_network.py
+++ prototypes/prototype_02/deep_q_network.py
@@ -8,18 +8,20 @@ import random
 
 
 class DeepQNetwork(nn.Module):
-    def __init__(self, lr, input_dims, fc1_dims, fc2_dims,
+    def __init__(self, lr, input_dims, fc1_dims, fc2_dims, fc3_dims,
                  n_actions):
         super(DeepQNetwork, self).__init__()
         self.input_dims = input_dims
         self.fc1_dims = fc1_dims
         self.fc2_dims = fc2_dims
+        self.fc3_dims = fc3_dims
         self.n_actions = n_actions
 
         # Layers
         self.fc1 = nn.Linear(self.input_dims, self.fc1_dims)
         self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
-        self.fc3 = nn.Linear(self.fc2_dims, self.n_actions)
+        self.fc3 = nn.Linear(self.fc2_dims, self.fc3_dims)
+        self.fc4 = nn.Linear(self.fc3_dims, self.n_actions)
 
         self.optimizer = torch.optim.Adam(self.parameters(), lr=lr)
         self.loss = nn.MSELoss()
@@ -29,5 +31,6 @@ class DeepQNetwork(nn.Module):
     def forward(self, state):
         x = F.relu(self.fc1(state))
         x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
         x = self.fc3(x)
         return x
