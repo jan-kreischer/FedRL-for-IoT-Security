c57b3d3c3b5e998a1a3784146d2c2cc707be1852
Jan Kreischer <jan.kr.bauer@gmail.com>
2023-06-09 23:53:04 +0200

    add full bdvl monitoring

---
+++ online_prototype_monitoring/main.py
@@ -0,0 +1,265 @@
+import time
+import os
+import subprocess
+import psutil
+import json
+import jsonschema
+from abc import ABC, abstractmethod
+from collections import deque
+from online_data_provider import OnlineDataProvider
+#from anomaly_detector import AutoEncoderInterpreter
+#from agent import Agent
+#import torch
+import numpy as np
+import pickle
+
+
+# TODO: add storage of agent networks after some episodes
+
+# TODO: make abstract (template method pattern) in case of multiple online methods
+class OnlineRL():
+    monitor_counter = 0
+    start_str_datafile = "online_samples_"
+
+    # start_str_datafile = "normal_samples_"
+
+    def __init__(self, ae=None, agent=None):  # AutoEncoderInterpreter=None, agent: Agent=None):
+        self.ae_interpreter = ae
+        self.agent = agent
+
+    # TODO: do something with learning data, progress monitoring
+    def activate_learning(self, interval=9000, monitor_duration=180):
+        print("start new monitoring loop every " + str(interval / 60) + "minutes")
+        episode_rewards = deque([0.0], maxlen=100)
+        steps = 0
+        episode = 0
+        while True:
+            print("episode: " + str(episode))
+            episode += 1
+            episode_reward, steps = self.start_monitoring_episode(steps, duration=monitor_duration)
+            if episode_reward:
+                episode_rewards.append(episode_reward)
+            print(episode_rewards)
+            print("!!!sleeping for " + str(interval) + "s, then finish early!!!")
+            time.sleep(interval)
+            exit(0)
+    # monitor ds for 1min, read, AD, c-action, launch mtd, wait, monitor as for 3h
+    def start_monitoring_episode(self, steps, duration):
+        print("testmonitor 100s")
+        self.monitor(180)
+        decision_data = self.read_data()
+        isAnomaly = self.interprete_data(decision_data)
+        if isAnomaly:
+            r = 0
+            while isAnomaly:
+                action = self.choose_action(decision_data)
+                self.launch_mtd(action)
+                print("sleep for 120s")
+                time.sleep(120)  # make dependent on pid of mtd script?
+                print("start monitoring afterstate")
+                self.monitor(14400)
+                after_data = self.read_data()
+
+                exit(0) # stop after monitoring afterstate
+
+                isAnomaly = self.interprete_data(after_data)
+                r += self.provide_feedback_and_update(decision_data, action, after_data, isAnomaly)
+                steps += 1
+                if steps % TARGET_UPDATE_FREQ == 0:
+                    self.agent.update_target_network()
+                print("steps: " + str(steps) + ", isAnomaly: " + str(isAnomaly))
+                print("terminating after a single agent update")
+                break
+                decision_data = after_data  # to reuse newly monitored data from afterstate
+            print("successfully mitigated attack using action: " + str(ACTIONS[action]) + "Step " + str(steps))
+            return r, steps
+        else:
+            return None, steps
+
+    def monitor(self, t: int = 180):
+        OnlineRL.monitor_counter += 1
+        # call monitoring shell script from python
+        print("running rl_sampler subprocess")
+        # subprocess.run(["./rl_sampler_online.sh", str(OnlineRL.monitor_counter)])
+        p = subprocess.Popen(["./rl_sampler_online.sh", str(OnlineRL.monitor_counter)])
+        print(p.pid)
+        time.sleep(t)
+        # killing all related processes
+        print("killing process")
+        print(p.pid)
+        kill(p.pid)
+
+    # TODO: adapt to monitor again if no samples left after filtering
+    def read_data(self):
+        """should only be used after running self.montitor(n) first, since it relies
+        on the monitor_counter to open the file online_samples_{c}"""
+        prefixed = [filename for filename in os.listdir('.') if
+                    filename.startswith(OnlineRL.start_str_datafile + str(OnlineRL.monitor_counter))]
+        print(prefixed)
+        assert len(prefixed) == 1, "Only one file with counter number should exist"
+        # read file and apply preprocessing
+        fname = prefixed[0]
+        print(fname)
+        print(os.getcwd())
+        data = OnlineDataProvider.get_scale_and_pca_transformed_data(fname)
+        print(data.shape)
+        return data
+
+    def interprete_data(self, data):
+        return True
+        # print("ae_interpreter threshold: " + str(ae_interpreter.threshold))
+        # flagged_anomalies = self.ae_interpreter.predict(data)
+        # print(flagged_anomalies)
+        # # return True
+        # return (torch.sum(flagged_anomalies).item() / len(flagged_anomalies)) > 0.5
+
+    def choose_action(self, data):
+        return 2
+        # print("action choice agent epsilon: " + str(self.agent.epsilon))
+        # actions = []
+        # if np.random.random() > self.agent.epsilon:
+        #     for state in data:
+        #         actions.append(self.agent.take_greedy_action(state))
+        #     print(actions)
+        #     return max(set(actions), key=actions.count)  # take action that the dqn predicts the most frequently
+        # else:
+        #     return np.random.choice(self.agent.action_space)
+
+    def launch_mtd(self, n: int):
+        print("Launching MTD " + ACTIONS[n])
+
+        with open('config.json') as json_file:
+            data = json.load(json_file)
+
+        # get commands from config file exactly:
+        selected_mtd = None
+        for mtd_conf in data[MTD_SOLUTIONS]:
+            if mtd_conf[SCRIPT_NAME] == ACTIONS[n]:
+                selected_mtd = mtd_conf
+        print(selected_mtd)
+        old_dir = os.getcwd()
+        print(old_dir)
+        os.chdir(selected_mtd[PATH])
+        print(os.getcwd())
+
+        mtd_params = ""
+        try:
+            mtd_params += str(selected_mtd[PARAMS])
+        except KeyError:
+            pass
+
+        print("run: " + selected_mtd[RUN_PREFIX] + ' ' + str(selected_mtd[SCRIPT_NAME]) + ' ' + mtd_params)
+        os.system(selected_mtd[RUN_PREFIX] + ' ' + str(selected_mtd[SCRIPT_NAME]) + ' ' + mtd_params)
+        os.chdir(old_dir)
+
+    def provide_feedback_and_update(self, decision_data, action, after_data, isAnomaly, max_len=10):
+        """feeds back [data] from afterstate behavior, along with a flag of whether it corresponds to normal behavior"""
+        print("provide feedback and update")
+        reward = -1 if isAnomaly else 1
+        done = not isAnomaly
+
+        # adding max=max_len monitored samples to replay_buffer
+        ld, la = len(decision_data), len(after_data)
+        m = min(ld, la)
+        n_samples = max_len if m > max_len else m
+        for i in range(n_samples):
+            self.agent.replay_buffer.append((np.expand_dims(decision_data[i, :], axis=0), action, reward,
+                                             np.expand_dims(after_data[i, :], axis=0), done))
+
+        # call agent.learn
+        self.agent.learn()
+        return reward
+
+
+def kill(pid):
+    '''Kills all process'''
+    parent = psutil.Process(pid)
+    for child in parent.children(recursive=True):
+        child.kill()
+    parent.kill()
+
+
+def validate_config_file(json_data):
+    with open('config-schema.json') as json_schema_file:
+        json_schema = json.load(json_schema_file)
+        try:
+            jsonschema.validate(instance=json_data, schema=json_schema, cls=jsonschema.Draft4Validator)
+        except jsonschema.ValidationError as err:
+            err.message = 'The config file does not follow the json schema and is therefore not compatible!'
+            raise err
+        return True
+
+
+# jsonschema data
+SCRIPT_NAME = 'ScriptName'
+PATH = 'RelativePath'
+TYPE = 'Type'
+MTD_SOLUTIONS = 'MTDSolutions'
+PARAMS = 'Params'
+RUN_PREFIX = 'RunWithPrefix'
+
+DIMS = 15
+# !!!Order is important!!!
+ACTIONS = ("ChangeIpAddress.py", "RemoveRootkit.py",
+           "CreateDummyFiles.py", "ChangeFileTypes.py")
+GAMMA = 0.99
+BATCH_SIZE = 100
+BUFFER_SIZE = 500
+MIN_REPLAY_SIZE = 100
+EPSILON_START = 1.0
+EPSILON_END = 0.01
+TARGET_UPDATE_FREQ = 100
+LEARNING_RATE = 1e-5
+N_EPISODES = 5000
+LOG_FREQ = 100
+
+# TODO: initialize agent with full memory buffer -> as per last episode of offline training
+if __name__ == '__main__':
+    with open('config.json') as json_file:
+        data = json.load(json_file)
+        validate_config_file(data)
+
+    # How it would look like in Online Monitoring/Training
+    # get pretrained anomaly detector
+    # pretrained_model = torch.load("trained_models/autoencoder_model.pth")
+    # ae_interpreter = AutoEncoderInterpreter(pretrained_model['model_state_dict'],
+    #                                         pretrained_model['threshold'], in_features=DIMS)
+    #
+    # # get pretrained agent with defined parameters
+    # pretrained_state = torch.load("trained_models/agent_0.pth")
+    # pretrained_agent = Agent(input_dims=DIMS, n_actions=len(ACTIONS), buffer_size=BUFFER_SIZE,
+    #                          batch_size=pretrained_state['batch_size'], lr=pretrained_state['lr'],
+    #                          gamma=pretrained_state['gamma'], epsilon=pretrained_state['eps'],
+    #                          eps_end=pretrained_state['eps_min'], eps_dec=pretrained_state['eps_dec'])
+    # pretrained_agent.online_net.load_state_dict(pretrained_state['online_net_state_dict'])
+    # pretrained_agent.target_net.load_state_dict(pretrained_state['target_net_state_dict'])
+    # pretrained_agent.replay_buffer = pretrained_state['replay_buffer']
+    #
+    # controller = OnlineRL(ae=ae_interpreter, agent=pretrained_agent)
+    #
+    # controller.activate_learning(interval=60, monitor_duration=100)
+
+
+    # remove before moving online
+    controller = OnlineRL()
+    controller.activate_learning(interval=60, monitor_duration=100)
+    #controller.monitor(100)
+    # #OnlineRL.monitor_counter += 1
+    #data = controller.read_data()
+    #
+    # # read the monitored data from file and apply all preset scalings and transforms
+    #decision_data = controller.read_data()
+    # after_data = controller.read_data()
+    #
+    # # run data through pretrained anomaly detector
+    # isAnomaly = controller.interprete_data(data)
+    # action = controller.choose_action(data)
+    # print("action")
+    # print(ACTIONS[action])
+    # controller.provide_feedback_and_update(decision_data, action, after_data, isAnomaly)
+
+    # print(isAnomaly)
+    # if isAnomaly:
+    #     action = controller.choose_action(decision_data)
+    #     print("chosen action: " + str(action))
+    #     controller.launch_mtd(action)
---
+++ online_prototype_monitoring/online_data_provider.py
@@ -0,0 +1,94 @@
+import pickle
+from typing import Dict
+from scipy import stats
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.decomposition import PCA
+import numpy as np
+import pandas as pd
+import os
+import joblib
+
+time_status_columns = ["time", "timestamp", "seconds", "connectivity"]
+all_zero_columns = ['cpuNice', 'cpuHardIrq', 'alarmtimer:alarmtimer_fired',
+                    'alarmtimer:alarmtimer_start', 'cachefiles:cachefiles_create',
+                    'cachefiles:cachefiles_lookup', 'cachefiles:cachefiles_mark_active',
+                    'dma_fence:dma_fence_init', 'udp:udp_fail_queue_rcv_skb']
+#
+#
+# ["alarmtimer:alarmtimer_fired", "alarmtimer:alarmtimer_start", "cachefiles:cachefiles_create",
+#                 "cachefiles:cachefiles_lookup", "cachefiles:cachefiles_mark_active", "dma_fence:dma_fence_init",
+#                 "udp:udp_fail_queue_rcv_skb"]
+
+perf_5_10_nan_columns = ['filemap:mm_filemap_add_to_page_cache', 'kmem:mm_page_alloc',
+                         'kmem:mm_page_alloc_zone_locked', 'kmem:mm_page_free', 'kmem:mm_page_pcpu_drain']
+perf_5_10_nan_columns += ['ramFree', 'ramUsed', 'writeback:writeback_write_inode', 'writeback:writeback_written']
+
+# choose which data to use for the scaler/pca
+all_file = "all_data_filtered_external_True_constant_True_outliers_True.csv"
+pca_file = "data_transforms/pcafit.gz"
+scaler_file = "data_transforms/scaler.gz"
+
+
+class OnlineDataProvider:
+    @staticmethod
+    def __parse_file_to_df(file_name, filter_suspected_external_events=True,
+                           filter_constant_columns=True,
+                           filter_outliers=True,
+                           keep_status_columns=False) -> pd.DataFrame:
+
+        df = pd.read_csv(file_name)
+
+        if filter_suspected_external_events:
+            # filter first 2 samples and last measurement
+            df = df.iloc[2:-1]
+        # filter for measurements where the device was connected
+        df = df[df['connectivity'] == 1]
+
+        # remove model-irrelevant columns
+        if not keep_status_columns:
+            df = df.drop(time_status_columns, axis=1)
+        # only in online setting nan columns that are not available as events
+        df = df.drop(perf_5_10_nan_columns, axis=1)
+
+        if filter_outliers:
+            # drop outliers per measurement, indicated by (absolute z score) > 3
+            df = df[(np.nan_to_num(np.abs(stats.zscore(df))) < 3).all(axis=1)]
+
+        if filter_constant_columns:
+            df = df.drop(all_zero_columns, axis=1)
+
+        # df.to_csv(file_name + "preprocessed", index_label=False)
+        return df
+
+    @staticmethod
+    def __get_scaler_and_pca(scaling_minmax=True, dim=15):
+        all_filtered_data = pd.read_csv(all_file).drop(perf_5_10_nan_columns, axis=1).to_numpy()[:, :-1].astype(np.float64)
+        scaler = StandardScaler() if not scaling_minmax else MinMaxScaler()
+        scaler.fit(all_filtered_data)
+        all_scaled = scaler.transform(all_filtered_data)
+        pca = PCA(n_components=dim)
+        pca.fit(all_scaled)
+
+        if not os.path.isfile(scaler_file) or not os.path.isfile(pca_file):
+            joblib.dump(scaler, scaler_file)
+            joblib.dump(pca, pca_file)
+            # with open(scaler_file, "wb") as sf:
+            #     pickle.dump(scaler, sf)
+            # with open(pca_file, "wb") as pf:
+            #     pickle.dump(pca, pf)
+        return scaler, pca
+
+    @staticmethod
+    def get_scale_and_pca_transformed_data(file_name):
+        data = OnlineDataProvider.__parse_file_to_df(file_name).values
+        if not os.path.isfile(pca_file) or not os.path.isfile(scaler_file):
+            scaler, pca = OnlineDataProvider.__get_scaler_and_pca()
+        else:
+            scaler = joblib.load(scaler_file)
+            pca = joblib.load(pca_file)
+            # with open('data_transforms/scaler.obj', 'rb') as sf:
+            #     scaler = pickle.load(sf)
+            # with open('data_transforms/pcafit.obj', 'rb') as pf:
+            #     pca = pickle.load(pf)
+
+        return pca.transform(scaler.transform(data))
