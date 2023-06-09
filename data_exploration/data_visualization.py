90989eaadb0d44345809e863c70ccf01e1c663ac
Jan Kreischer <jan.kr.bauer@gmail.com>
2023-06-09 23:53:05 +0200

    wip add ransom data, wip data kdeplots

--- data_exploration/data_visualization.py
+++ data_exploration/data_visualization.py
@@ -7,28 +7,29 @@ plot_kde = True
 plot_timeline = False
 
 if __name__ == "__main__":
-    if plot_kde:pass
-        #DataPlotter.plot_delay_and_normal_as_kde()
-        DataPlotter.plot_behaviors_as_kde()
-        #DataPlotter.plot_devices_as_kde_pub()
+    if plot_kde:
+        # DataPlotter.plot_delay_and_normal_as_kde()
+        # DataPlotter.plot_behaviors_as_kde(RaspberryPi.PI4_2GB_WC)
+        DataPlotter.plot_devices_as_kde(RaspberryPi.PI4_2GB_WC)
+
     # UserWarning: Logscale warning can be ignored (some samples have negative values for feature iface0TX)
     if plot_timeline:
         DataPlotter.plot_behaviors(
             [(RaspberryPi.PI4_2GB_WC, Behavior.NORMAL, "blue"),
              (RaspberryPi.PI4_2GB_WC, Behavior.ROOTKIT_BDVL, "orange"),
              (RaspberryPi.PI4_2GB_WC, Behavior.ROOTKIT_BEURK, "green"),
-             #(RaspberryPi.PI4_2GB_WC, Behavior.CNC_THETICK, "yellow"),
-             #(RaspberryPi.PI4_2GB_WC, Behavior.CNC_BACKDOOR_JAKORITAR, "cyan"),
-             #(RaspberryPi.PI4_2GB_WC, Behavior.RANSOMWARE_POC, "black")
+             # (RaspberryPi.PI4_2GB_WC, Behavior.CNC_THETICK, "yellow"),
+             # (RaspberryPi.PI4_2GB_WC, Behavior.CNC_BACKDOOR_JAKORITAR, "cyan"),
+             # (RaspberryPi.PI4_2GB_WC, Behavior.RANSOMWARE_POC, "black")
              ], plot_name="normal_rootkit_pi_4_2gb_timeline")
 
         DataPlotter.plot_behaviors(
             [(RaspberryPi.PI4_2GB_WC, Behavior.NORMAL, "blue"),
              (RaspberryPi.PI4_2GB_WC, Behavior.CNC_THETICK, "yellow"),
-             (RaspberryPi.PI4_2GB_WC, Behavior.CNC_BACKDOOR_JAKORITAR, "cyan"),
+             (RaspberryPi.PI4_2GB_WC, Behavior.CNC_BACKDOOR_JAKORITAR, "cyan")
              ], plot_name="normal_cnc_pi_4_2gb_timeline")
 
         DataPlotter.plot_behaviors(
             [(RaspberryPi.PI4_2GB_WC, Behavior.NORMAL, "blue"),
-             (RaspberryPi.PI4_2GB_WC, Behavior.RANSOMWARE_POC, "black"),
+             (RaspberryPi.PI4_2GB_WC, Behavior.RANSOMWARE_POC, "black")
              ], plot_name="normal_ransom_pi_4_2gb_timeline")
---
+++ test2.py
@@ -0,0 +1,6 @@
+import time
+
+
+print("inside test2! sleep 3s")
+time.sleep(5)
+print("inside test2! after sleep 3s")
